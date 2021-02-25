c3ddb38629f4d785d1e8e7f7f8f5a646d34437e4
zubayra <zubayraai@gmail.com>
2021-02-25 13:09:19 -0500

    first commit

---
+++ src/GitParactice/git_practice01.java
@@ -0,0 +1,11 @@
+package GitParactice;
+
+public class git_practice01 {
+    // use print I am "Zubayra" and I hate "Nigmat", 4space 'Hello Word\'
+    // Do not use space
+    //hint: 1 tab=2 space
+
+    public static void main(String[] args){
+        System.out.println("I am \"Zubayra\" and I love \"Nigmat\",\n \t\t\'Hello Word\\\'");
+    }
+}
