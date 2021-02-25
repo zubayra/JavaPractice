b4f1e3476b01618d9e4d8fa13fdd8ba84f677afc
zubayra <zubayraai@gmail.com>
2021-02-25 13:40:26 -0500

    Change in demo1 class

--- src/Demo/demo1.java
+++ src/Demo/demo1.java
@@ -1,4 +1,8 @@
 package Demo;
 
 public class demo1 {
+    // Write something in demo1 to indicate how to push
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
